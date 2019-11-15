1fd767efb13010184153ba494d64ce71d02d50a4
BH04902 <daniel.barrios@macys.com>
2019-11-14 23:01:19 -0300

    taskbox UI

---
+++ .storybook/config.js
@@ -0,0 +1,13 @@
+import { configure } from '@storybook/vue';
+
+import '../src/index.css';
+
+const req = require.context('../src', true, /\.stories.js$/);
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+// automatically import all files ending in *.stories.js
+configure(loadStories, module);
+
+// configure(require.context('../stories', true, /\.stories\.js$/), module);
